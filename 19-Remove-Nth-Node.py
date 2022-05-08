259eef95efd322e3fe5b0377cd8745420000570f
Jared Connor <jaredconnor301@gmail.com>
2022-05-07 21:13:06 -0700

    Adding 19-Remove-nth-node

---
+++ 19-Remove-Nth-Node.py
@@ -0,0 +1,26 @@
+# Definition of singly-linked list
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+
+        fast = slow = head
+
+        for i in range(n):
+            fast = fast.next
+
+        if fast is None:
+            return head.next
+
+        while fast.next is not None:
+            fast = fast.next
+            slow = slow.next
+        else:
+            slow.next = slow.next.next
+
+        return head

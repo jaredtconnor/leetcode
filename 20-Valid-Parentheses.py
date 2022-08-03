fd6e928f687343f4de111e5ebed6fde5211ec813
Jared Connor <jaredconnor301@gmail.com>
2022-08-03 09:35:36 -0700

    Adding Valid Parenthesis

--- 20-Valid-Parentheses.py
+++ 20-Valid-Parentheses.py
@@ -1,23 +1,23 @@
 
-
 class Solution:
     def isValid(self, s: str) -> bool:  
 
         # Create our stack datastructure 
-        stack = []
+        stack = [] 
+        valid_starts = ['{', '[','(']
 
         # Loop through each character in the string 
         for c in s:  
 
             # If the string is a starting element, add to our stack 
-            if c in ["{", "(", "["]: 
+            if c in valid_starts: 
                 stack.append(c)
             
             # Else if the character is a right element and the left element is one before it, remove from stack as valid 
             elif c == ')' and stack[-1] == '(' and len(stack) != 0: 
                 stack.pop()
 
-            elif c == ']' and stack[-1] == ']' and len(stack) != 0: 
+            elif c == ']' and stack[-1] == '[' and len(stack) != 0: 
                 stack.pop()
 
             elif c == '}' and stack[-1] == '{' and len(stack) != 0: 
@@ -34,6 +34,11 @@ class Solution:
 solution = Solution()
 
 # Test cases: 
-s = "()" 
-print(f"Result: {solution.isValid(s)}")
+s_1 = "()"   
+s_2 = "(){}[]" 
+s_3 = "(]"
+
+print(f"Result for '{s_1}': {solution.isValid(s_1)} | Should be True")
+print(f"Result for '{s_2}': {solution.isValid(s_2)} | Should be True")
+print(f"Result for '{s_3}': {solution.isValid(s_3)} | Should be False")
 
